c28dac3fd336b376dcfb4efef9501c62266cb0bd
張凱鈞 <thisisbirdhead@gmail.com>
2019-09-16 22:30:05 +0800

    安裝自製套件hello

--- config/app.php
+++ config/app.php
@@ -161,6 +161,7 @@ return [
         Illuminate\Translation\TranslationServiceProvider::class,
         Illuminate\Validation\ValidationServiceProvider::class,
         Illuminate\View\ViewServiceProvider::class,
+        Thisisbird\Hello\HelloServiceProvider::class,
 
         /*
          * Package Service Providers...
